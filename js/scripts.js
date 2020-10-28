395a6746ffe4cd9c6c004c31a29ab04ddec6983a
Leidy Lorena Cita <llcita@misena.edu.co>
2020-10-28 11:19:06 -0500

    Proceso 1

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+setTimeout(function () {
+    document.getElementById('fondo').style.transform="scale(0)";
+}, 4000);
+setTimeout(function () {
+    document.getElementById('pre-loader').style.transform="scale(0)";
+}, 4000);
+
+
